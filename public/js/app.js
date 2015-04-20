6353e05b27dcdca1bd20868af28d172b8a169b00
ami-kumar <ak3284@columbia.edu>
2015-04-20 10:32:25 -0400

    ng-class

--- public/js/app.js
+++ public/js/app.js
@@ -27,38 +27,8 @@ var app = angular.module('flashCards', []);
 //     }
 // ]);
 
-// create an Angular factory that will make an AJAX request
-//  to the server
-app.factory('FlashCardsFactory', function($http) {
-    return { 
-        getFlashCards: function(category) {
-            var queryParams = {}
-
-            if (category) {
-                queryParams.category = category;
-            }
-            // build an AJAX call
-            return $http.get("/cards", {
-                params: queryParams
-            }).then(function(response) {
-                return response.data
-            })
-        }
+app.filter('name', function() {
+    return function(s) {
+        return s
     }
 })
-
-app.factory('ScoreFactory', function () {
-    // facilitate communication between StatsController and flash card
-    //  controllers
-
-    return {
-        correct: 0,
-        incorrect: 0
-    }
-})
-
-app.controller('StatsController', function ($scope, ScoreFactory) {
-    $scope.scores = ScoreFactory;
-    console.log($scope.scores)
-});
-
