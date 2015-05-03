fe1c5e71f93993b9fe73c3a61caf0dd3cadbbdc5
ami-kumar <ak3284@columbia.edu>
2015-05-03 17:16:23 -0400

    html for all flashcards in flashcard.html

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,77 +1,77 @@
 app.controller('MainController', function ($scope, FlashCardsFactory, ScoreFactory) {
-	FlashCardsFactory.getFlashCards().then(function(cards) {
-		$scope.flashCards = cards
-	})
-	console.log($scope.flashCards)
+	// FlashCardsFactory.getFlashCards().then(function(cards) {
+	// 	$scope.flashCards = cards
+	// })
+	// console.log($scope.flashCards)
 
-	$scope.categories = [
-		'MongoDB', 
-		'Express', 
-		'Angular',
-		'Node'
-	]
+	// $scope.categories = [
+	// 	'MongoDB', 
+	// 	'Express', 
+	// 	'Angular',
+	// 	'Node'
+	// ]
 
-	$scope.answerQuestion = function (answer, flashCard) {
-		if (!flashCard.answered) {
-			flashCard.answered = true;
-			flashCard.answeredCorrectly = answer.correct;
-		}
-		if (answer.correct) {
-			ScoreFactory.correct++
-		} else {
-			ScoreFactory.incorrect++
-		}
-	}
+	// $scope.answerQuestion = function (answer, flashCard) {
+	// 	if (!flashCard.answered) {
+	// 		flashCard.answered = true;
+	// 		flashCard.answeredCorrectly = answer.correct;
+	// 	}
+	// 	if (answer.correct) {
+	// 		ScoreFactory.correct++
+	// 	} else {
+	// 		ScoreFactory.incorrect++
+	// 	}
+	// }
 
-	$scope.getCategoryCards = function(category) {
-		console.log(category)
-		// if there is a category, add it to the $http call
-		//	to the server
+	// $scope.getCategoryCards = function(category) {
+	// 	console.log(category)
+	// 	// if there is a category, add it to the $http call
+	// 	//	to the server
 
-		// implement call to FlashCardFactory.getFlashCards
-		// 	take filtered flash cards, replace the existing
-		//	flashCards on the $scope
-		FlashCardsFactory.getFlashCards(category).then(function(cards) {
-			$scope.flashCards = cards
-			$scope.category = category
+	// 	// implement call to FlashCardFactory.getFlashCards
+	// 	// 	take filtered flash cards, replace the existing
+	// 	//	flashCards on the $scope
+	// 	FlashCardsFactory.getFlashCards(category).then(function(cards) {
+	// 		$scope.flashCards = cards
+	// 		$scope.category = category
 
-			console.log(cards)
-			console.log(category)
-		})
-	}
+	// 		console.log(cards)
+	// 		console.log(category)
+	// 	})
+	// }
 
-	$scope.activeCategory = function(category) {
-		if (category == $scope.category) {
-			return true
-		} else {
-			return false
-		}
-	}
+	// $scope.activeCategory = function(category) {
+	// 	if (category == $scope.category) {
+	// 		return true
+	// 	} else {
+	// 		return false
+	// 	}
+	// }
 
-	$scope.resetCategory = function() {
-		FlashCardsFactory.getFlashCards().then(function(cards) {
-			$scope.flashCards = cards
-		})
-		console.log($scope.flashCards)
-	}
+	// $scope.resetCategory = function() {
+	// 	FlashCardsFactory.getFlashCards().then(function(cards) {
+	// 		$scope.flashCards = cards
+	// 	})
+	// 	console.log($scope.flashCards)
+	// }
 
-	$scope.flashCardsLoaded = function() {
-		if ($scope.flashCards) {
-			console.log("flashcards have been loaded")
-			return true
-		} else {
-			console.log("flashcards have not been loaded")
-			return false
-		}
-	}
+	// $scope.flashCardsLoaded = function() {
+	// 	if ($scope.flashCards) {
+	// 		console.log("flashcards have been loaded")
+	// 		return true
+	// 	} else {
+	// 		console.log("flashcards have not been loaded")
+	// 		return false
+	// 	}
+	// }
 
-	$scope.flashCardsNotLoaded = function() {
-		if ($scope.flashCards) {
-			console.log("Take off loader")
-			return false
-		} else {
-			console.log("Keep on loader")
-			return true
-		}
-	}
+	// $scope.flashCardsNotLoaded = function() {
+	// 	if ($scope.flashCards) {
+	// 		console.log("Take off loader")
+	// 		return false
+	// 	} else {
+	// 		console.log("Keep on loader")
+	// 		return true
+	// 	}
+	// }
 });
