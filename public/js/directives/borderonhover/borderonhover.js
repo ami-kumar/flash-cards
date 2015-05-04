30e00dfc63f0cf6c7542535f234ea3b40c10cf93
ami-kumar <ak3284@columbia.edu>
2015-05-04 18:56:03 -0400

    first draft

--- public/js/directives/borderonhover/borderonhover.js
+++ public/js/directives/borderonhover/borderonhover.js
@@ -3,19 +3,26 @@ app.directive('borderonhover', function() {
 		restrict: 'A',
 		templateUrl: 'js/directives/borderonhover/borderonhover.html',
 		link: function(scope, element) {
+			// element.on('mouseenter', function() {
+			// 	// apply 2px solid border to element
+			// 	scope.$apply(function() {
+			// 		console.log(element)
+			// 		console.log(element.html())
+			// 	} 
+			// 	console.log(element.html())
+			// 	console.log('is this working?')
+			// })
+			// element.on('mouseover', function() {
+			// 	// take away 2 px solid border to element
+			// 	console.log('yay')
+			// })
+
 			element.on('mouseenter', function() {
-				// apply 2px solid border to element
-				scope.$apply(function() {
-					console.log(element)
-					console.log(element.html())
-				} 
-				console.log(element.html())
-				console.log('is this working?')
-			})
-			element.on('mouseover', function() {
-				// take away 2 px solid border to element
-				console.log('yay')
-			})
+                element.addClass( "border" );
+            });
+            element.on('mouseleave', function() {
+				element.removeClass( "border" );
+            });
 		}
 	}
 }))
